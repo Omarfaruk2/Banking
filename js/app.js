3f9c8f1b2b4484aa93aafb96930d4e3f29d36d62
Omarfaruk4200 <emon35293@gmail.com>
2022-02-17 23:19:12 +0600

    added eror handle

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 
+//----------------------------------------- Input function-----------------
 function getinput(money) {
+    const out = 2000;
     const input = document.getElementById(money + '-input');
     const inputvalueText = input.value;
     const inputValue = parseInt(inputvalueText);
-    if (isNaN(input.value) || input.value < 0) {
-        alert('hello')
+    if (isNaN(input.value) || input.value < 0 || input.value == '') {
+        inputValue == out;
+        return alert("Your number isn't a valid, Input a valid number");
     }
-    return inputValue;
+    else {
+        return inputValue;
+    }
 }
+
+// -------------------------------------input-Itext-function----------------------------------
 function getText(jan) {
     const textValue = document.getElementById(jan + '-money')
     return textValue;
 }
+
+// -------------------------------------------calculate -Button----------------------------
 document.getElementById('calculate-btn').addEventListener('click',
     function () {
         const income = getinput('income');
@@ -19,35 +28,38 @@ document.getElementById('calculate-btn').addEventListener('click',
         const rent = getinput('rent');
         const clothes = getinput('clothes');
         const total = food + rent + clothes;
-
-        const hello = getText('totalExpence')
-        hello.innerText = total;
-
-        let avilableMoney = getText('avilable');
-        const avilable = income - total;
-        avilableMoney.innerText = avilable
+        const useMoney = getText('totalExpence')
+        if (useMoney.innerText > income) {
+            alert('Your Expence is more then Income, Try to expence less then income');
+        }
+        else {
+            useMoney.innerText = total;
+            let avilableMoney = getText('avilable');
+            const avilable = income - total;
+            avilableMoney.innerText = avilable
+        }
 
     })
+//   --------------------------------------------------save -Button---------------------------------
 document.getElementById('save-btn').addEventListener('click',
     function () {
+        let moneyValue = 00;
         let avilableMoney = getText('avilable');
+        const income = getinput('income');
         const saveInput = getinput('save');
-        const savingAmountValue = avilableMoney.innerText * saveInput / 100;
-
+        const savingAmountValue = income * saveInput / 100;
         const savingAmount = getText('saving')
         savingAmount.innerText = savingAmountValue;
-
         const remainingAmount = getText('remaiening');
-        remainingAmount.innerText = avilableMoney.innerText - savingAmount.innerText;
 
-        // console.log(remainingAmount.innerText, 'hello')
-
-
+        if (avilableMoney.innerText < savingAmount.innerText) {
+            remainingAmount.innerText = moneyValue;
+            savingAmount.innerText = moneyValue;
+            return alert("You don't have enough money to saving");
+        }
+        else {
+            remainingAmount.innerText = avilableMoney.innerText - savingAmount.innerText;
+        }
     })
-
-
-
-
-
 
 
